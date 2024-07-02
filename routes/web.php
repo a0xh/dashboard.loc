8b4ef5389c43dfed42b2553688c88325c0101d7b
Vladislav Malikov <a0xh@4ifi.ru>
2024-07-02 08:29:10 +0500

    Adding Automatic Authorisation

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,11 @@ Auth::routes([
 	'confirm' => true,
 	'verify' => true
 ]);
+
+Route::get('/login', function() {
+	abort_unless(app()->environment('local'), 403);
+
+	auth()->login(\App\Domain\User\Domain\User::first());
+
+	return redirect()->to('/admin');
+});
